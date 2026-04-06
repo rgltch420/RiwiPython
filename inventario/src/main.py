938b48c672ec0f2a18d9261650b19f98bc11e7e4
rgltch420 <rmelendez.ruiz@icloud.com>
2026-04-06 15:44:20 -0500

    ORDER MENU

--- inventario/src/main.py
+++ inventario/src/main.py
@@ -4,10 +4,10 @@ while True:
     ascii_art()
         
     print("1. Add product")
-    print("2. Show Inventary")
-    print("3. Calculate estadistics")
-    print("4. Update Inventory")
-    print("5. Delete Inventory")
+    print("2. Update Product")
+    print("3. Delete Product")
+    print("4. Calculate estadistics")
+    print("5. Show Inventary")
     print("6. Save Data")
     print("7. Load Data")
     print("8. Exit")
@@ -19,14 +19,14 @@ while True:
     match option:
         case "1":
             create_product()
-        case "2":    
-            show_inventory()
-        case "3":
-            stadistics()
-        case "4":
+        case "2":
             update_products()
-        case "5":
+        case "3":
             delete_products()  
+        case "4":
+            stadistics()
+        case "5":    
+            show_inventory()
         case "6":
             save_data()
         case "7":
